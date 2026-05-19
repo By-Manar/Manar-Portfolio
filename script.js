0a77510e7c22cf6adc6429d90988411b60b76fb1
Manar Alghamdi <manar995511@gmail.com>
2026-05-19 16:25:00 +0300

    updated portgolio
    :wq

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-script.js
 const menuBtn = document.getElementById("menuBtn");
 const navMenu = document.getElementById("navMenu");
 
@@ -11,18 +9,4 @@ document.querySelectorAll(".nav-menu a").forEach(link => {
   link.addEventListener("click", () => {
     navMenu.classList.remove("active");
   });
-=======
-script.js
-const menuBtn = document.getElementById("menuBtn");
-const navMenu = document.getElementById("navMenu");
-
-menuBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-menu a").forEach(link => {
-  link.addEventListener("click", () => {
-    navMenu.classList.remove("active");
-  });
->>>>>>> fbe6f911c85749c0a6cbbc00ac3ae680f31e2df9
 });
